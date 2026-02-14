3cd1e437102b7a40cf1ed60ad57dedfd4ce55724
Namchai Hangkasee <s6604062620158@email.kmutnb.ac.th>
2026-02-15 00:26:06 +0700

    Commit 6 >> Lab8no3.c: Sol2: Binary search

--- Lab8no3.c
+++ Lab8no3.c
@@ -24,20 +24,20 @@ int main() {
 
     srand(time(NULL));
     
-   
+    
     for (i = 0; i < N; i++) {
         A[i] = checkNum(A, i);
     }
 
     printf("\nFinal Array:\n");
     printAr(A, N);
-    printf("\n");
+   
 
     return 0;
 }
 
 void printAr(int numAr[], int numArSize) {
-	int i;
+		int i;	
     for ( i = 0; i < numArSize; i++)
         printf("%d ", numAr[i]);
 }
@@ -52,21 +52,37 @@ int checkNum(int numAr[], int numArSize) {
     return rnum;
 }
 
-// --- Sol 1: Linear Search ---
-int searchNum(int numAr[], int numArSize, int target) {
-    int i, found = 0;
-    for (i = 0; i < numArSize; i++) {
-        if (numAr[i] == target) {
-            found = 1; 
-            break;
-        }
+void sortAr(int numAr[], int size) {
+    int i, j, min_idx, temp;
+    for (i = 0; i < size-1; i++) {
+        min_idx = i;
+        for (j = i+1; j < size; j++)
+            if (numAr[j] < numAr[min_idx]) min_idx = j;
+        temp = numAr[min_idx];
+        numAr[min_idx] = numAr[i];
+        numAr[i] = temp;
     }
-    return found;
 }
+
+// --- Sol 2: Binary Search ---
+int searchNum(int numAr[], int numArSize, int target) {
+   
+    sortAr(numAr, numArSize); 
+    
+    int low = 0, high = numArSize - 1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (numAr[mid] == target) return 1;
+        if (numAr[mid] < target) low = mid + 1;
+        else high = mid - 1;
+    }
+    return 0;
+}
+
 
 int random1to100() {
     int random_num = (rand() % 100) + 1;
-  
+
     printf("[%d] ", random_num); 
     return random_num;
 }
